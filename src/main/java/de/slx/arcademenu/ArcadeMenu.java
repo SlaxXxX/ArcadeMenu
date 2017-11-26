c3ebbf948600d7b937e7653af1fcbfccb4974d0e
SlaxXxX <Janis.Schneider97@web.de>
2017-11-26 11:36:26 +0100

    javaFX is working; back to master

--- src/main/java/de/slx/arcademenu/ArcadeMenu.java
+++ src/main/java/de/slx/arcademenu/ArcadeMenu.java
@@ -1,26 +1,50 @@
 package de.slx.arcademenu;
 
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-import javax.swing.*;
 import java.io.File;
 
-public class ArcadeMenu extends JFrame {
+public class ArcadeMenu extends Application {
+
     String musicPath = "src/main/resources/Music.mp3";
 
     public static void main(String[] args) {
-        new ArcadeMenu();
+        launch(args);
     }
 
-    public ArcadeMenu(){
-        //this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        this.setUndecorated(true);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //DO_NOTHING_ON_CLOSE
-
+    @Override
+    public void init() {
         Media music = new Media(new File(musicPath).toURI().toString());
         MediaPlayer player = new MediaPlayer(music);
         player.play();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root);
+        scene.setCursor(Cursor.NONE);
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.setOnCloseRequest(Event::consume);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+
     }
 }
