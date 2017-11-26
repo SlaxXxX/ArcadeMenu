9a8d3ba1a98de9fc694941c5b4f919af14340b2b
SlaxXxX <Janis.Schneider97@web.de>
2017-11-26 10:56:12 +0100

    jframe might not do the job; switching to javafx

--- src/main/java/de/slx/arcademenu/ArcadeMenu.java
+++ src/main/java/de/slx/arcademenu/ArcadeMenu.java
@@ -1,6 +1,26 @@
 package de.slx.arcademenu;
 
-public class ArcadeMenu {
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-    
+import javax.swing.*;
+import java.io.File;
+
+public class ArcadeMenu extends JFrame {
+    String musicPath = "src/main/resources/Music.mp3";
+
+    public static void main(String[] args) {
+        new ArcadeMenu();
+    }
+
+    public ArcadeMenu(){
+        //this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(true);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //DO_NOTHING_ON_CLOSE
+
+        Media music = new Media(new File(musicPath).toURI().toString());
+        MediaPlayer player = new MediaPlayer(music);
+        player.play();
+    }
 }
