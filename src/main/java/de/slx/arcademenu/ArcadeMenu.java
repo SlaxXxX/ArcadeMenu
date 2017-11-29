45270d3239d97dd8d5b2890deec24f1d3bd49b9b
SlaxXxX <Janis.Schneider97@web.de>
2017-11-29 10:26:06 +0100

    2.1.0: finally working ui

--- src/main/java/de/slx/arcademenu/ArcadeMenu.java
+++ src/main/java/de/slx/arcademenu/ArcadeMenu.java
@@ -12,7 +12,7 @@ import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.scene.image.ImageView;
-import javafx.animation.PathTransition;
+import javafx.animation.*;
 import javafx.scene.*;
 import javafx.event.*;
 
@@ -30,17 +30,21 @@ public class ArcadeMenu extends Application {
 	ArrayList<Element> games = new ArrayList<>();
 	StackPane layout;
 
-	int[] angles = new int[] { 7, 19, 37, 55, 67, 79 };
-	int removeElement = -1;
-	int elementCount = 6;
+	int[] angles = new int[] { -8, 7, 19, 35, 55, 67, 79, 91 };
+	int pathFinished = 0;
+	int elementCount = 8;
+	int elementIndex = 0;
 
-	int delay = 1000;
-	int climbingDelay = delay;
+	int duration = 150;
+	int climbingDuration = duration;
 	double climbSpeed = 0.98;
-	int minDelay = 40;
+	int minDelay = 60;
 
 	final Dimension d = new Dimension(1024, 768);
 	final int radius = (int) (d.width / 4 * 2.5);
+	
+	ScaleTransition upscaleTransition = new ScaleTransition();
+	ScaleTransition downscaleTransition = new ScaleTransition();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -74,6 +78,15 @@ public class ArcadeMenu extends Application {
 		//		 Media music = new Media(dataFolder.toURI().toString() + "/Music.mp3");
 		//		 MediaPlayer player = new MediaPlayer(music);
 		//		 player.play();
+		
+				upscaleTransition.setFromX(1);
+				upscaleTransition.setFromY(1);
+				upscaleTransition.setToX(1.8);
+				upscaleTransition.setToY(1.8);
+				downscaleTransition.setFromX(1.8);
+				downscaleTransition.setFromY(1.8);
+				downscaleTransition.setToX(1);
+				downscaleTransition.setToY(1);
 	}
 
 	private String strip(String s) {
@@ -89,10 +102,10 @@ public class ArcadeMenu extends Application {
 		games.forEach(element -> layout.getChildren().add(element.image));
 
 		Scene scene = new Scene(layout, d.width, d.height);
-		scene.setCursor(Cursor.NONE);
+		//		scene.setCursor(Cursor.NONE);
 		scene.addEventFilter(KeyEvent.KEY_PRESSED, this::pressedKey);
 		scene.setOnKeyReleased(ke -> {
-			climbingDelay = delay;
+			climbingDuration = duration;
 		});
 		stage.setScene(scene);
 
@@ -101,11 +114,17 @@ public class ArcadeMenu extends Application {
 		// stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
 		// stage.setOnCloseRequest(Event::consume);
 		stage.show();
+
 		for (int i = 0; i < games.size(); i++) {
-			games.get((i + games.size() - 2) % games.size()).image.relocate(radius, d.height + games.get(i).image.getFitHeight() / 2);
+			games.get((i + games.size() - 3) % games.size()).image.relocate(radius, d.height + games.get(i).image.getFitHeight() / 2);
 			if (i < elementCount)
-				games.get((i + games.size() - 2) % games.size()).setPath(i, i * 150, 1000);
+				games.get((i + games.size() - 3) % games.size()).setPath(i, i * 150, 1000);
+
 		}
+		upscaleTransition.setNode(games.get(getIndex(3)).image);
+		upscaleTransition.setDuration(Duration.millis(1150));
+		upscaleTransition.play();
+
 	}
 
 	private int add(int num, int add, int max) {
@@ -124,28 +143,48 @@ public class ArcadeMenu extends Application {
 			} catch (Exception ex) {
 			}
 		}
-		if (e.getCode() == KeyCode.UP && removeElement == -1) {
-			removeElement = 0;
-			games.add(new Element(games.get(0)));
-			layout.getChildren().add(games.get(games.size() - 1).image);
-			games.get(games.size() - 1).image.relocate(radius, d.height + games.get(games.size() - 1).image.getFitHeight() / 2);
+		if (e.getCode() == KeyCode.UP && pathFinished == elementCount) {
+			elementIndex = add(elementIndex, -1, games.size());
+			pathFinished = 0;
+
+//			games.get(getIndex(elementCount)).setPath(getIndex(elementCount), 0, climbingDelay, new Point(radius, (int)(d.height + games.get(getIndex(elementCount)).image.getFitHeight() / 2)));
 			for (int i = 0; i < elementCount; i++) {
-				games.get((i + elementCount - 2) % elementCount).setPath(i, 0, climbingDelay);
+				games.get(getIndex(i)).setPath(i, 0, climbingDuration);
 			}
-			if (climbingDelay > minDelay)
-				climbingDelay *= climbSpeed;
+			
+			upscaleTransition.setNode(games.get(getIndex(3)).image);
+			downscaleTransition.setNode(games.get(getIndex(4)).image);
+			upscaleTransition.setDuration(Duration.millis(climbingDuration));
+			downscaleTransition.setDuration(Duration.millis(climbingDuration));
+			upscaleTransition.play();
+			downscaleTransition.play();
+
+			if (climbingDuration > minDelay)
+				climbingDuration *= climbSpeed;
 		}
-		if (e.getCode() == KeyCode.DOWN && removeElement == -1) {
-			removeElement = games.size();
-			games.add(0, new Element(games.get(games.size() - 1)));
-			layout.getChildren().add(games.get(0).image);
-			games.get(0).image.relocate(0, radius + games.get(0).image.getFitHeight() / 2);
+		if (e.getCode() == KeyCode.DOWN && pathFinished == elementCount) {
+			elementIndex = add(elementIndex, 1, games.size());
+			pathFinished = 0;
+			
+//			games.get(getIndex(0)).setPath(getIndex(0), 0, climbingDelay, new Point(radius, (int)(d.height + games.get(getIndex(0)).image.getFitHeight() / 2)));
 			for (int i = 0; i < elementCount; i++) {
-				games.get((i + elementCount - 2) % elementCount).setPath(i, 0, climbingDelay);
+				games.get(getIndex(i)).setPath(i, 0, climbingDuration);
 			}
-			if (climbingDelay > minDelay)
-				climbingDelay *= climbSpeed;
+			
+			upscaleTransition.setNode(games.get(getIndex(3)).image);
+			downscaleTransition.setNode(games.get(getIndex(2)).image);
+			upscaleTransition.setDuration(Duration.millis(climbingDuration));
+			downscaleTransition.setDuration(Duration.millis(climbingDuration));
+			upscaleTransition.play();
+			downscaleTransition.play();
+			
+			if (climbingDuration > minDelay)
+				climbingDuration *= climbSpeed;
 		}
+	}
+	
+	private int getIndex(int iterator) {
+		return ((elementIndex + (iterator + elementCount - 3) % elementCount) % games.size());
 	}
 
 	@Override
@@ -179,24 +218,33 @@ public class ArcadeMenu extends Application {
 				image.setFitWidth(100);
 			} catch (Exception e) {
 			}
+			image.setAccessibleText(name);
 		}
 
-		private Point getPos(Node node) {
-			return new Point((int) (node.getLayoutX() + node.getTranslateX()), (int) (node.getLayoutY() + node.getTranslateY()));
+		private Point getLocal() {
+			return new Point((int) (image.getTranslateX() + image.getFitWidth() / 2), (int) (image.getTranslateY() + image.getFitHeight() / 2));
 		}
 
-		private Point getDelta(ImageView image, int pos) {
-			Point middle = new Point(getPos(image).x - radius, getPos(image).y);
+		private Point getPos() {
+			return new Point((int) (image.getLayoutX() + image.getTranslateX() + image.getFitWidth() / 2), (int) (image.getLayoutY() + image.getTranslateY() + image.getFitHeight() / 2));
+		}
+
+		private Point getDelta(int pos) {
+			Point middle = new Point(getPos().x - radius, getPos().y);
 			Point stop = new Point(middle.x + (int) (radius * Math.cos(Math.toRadians(270) + Math.toRadians(angles[pos]))), middle.y + (int) (radius * Math.sin(Math.toRadians(270) + Math.toRadians(angles[pos]))));
-			return new Point(stop.x - getPos(image).x, stop.y - getPos(image).y);
+			return new Point(stop.x - getPos().x, stop.y - getPos().y);
 		}
 
 		private void setPath(int pos, int delay, int duration) {
+			setPath(pos, delay, duration, getPos());
+		}
+
+		private void setPath(int pos, int delay, int duration, Point start) {
 			PathTransition pathTransition = new PathTransition();
 
 			Path path = new Path();
-			Point delta = getDelta(image, pos);
-			path.getElements().add(new MoveTo(image.getTranslateX() + image.getFitWidth() / 2, image.getTranslateY() + image.getFitHeight() / 2));
+			Point delta = getDelta(pos);
+			path.getElements().add(new MoveTo(start.x - getPos().x + getLocal().x, start.y - getPos().y + getLocal().y));
 
 			ArcTo arcTo = new ArcTo(radius, radius,
 					0, delta.x, delta.y, false, false);
@@ -208,20 +256,7 @@ public class ArcadeMenu extends Application {
 			pathTransition.setNode(image);
 			pathTransition.setPath(path);
 
-			pathTransition.setOnFinished(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event) {
-					if (removeElement != -1) {
-						games.remove(removeElement);
-						removeElement = -1;
-					}
-					if (event.getSource() instanceof PathTransition) {
-						PathTransition source = (PathTransition) event.getSource();
-						Node node = source.getNode();
-						System.out.println(node.getLayoutX() + " / " + node.getTranslateX() + " || " + node.getLayoutY() + " / " + node.getTranslateY());
-					}
-				}
-			});
+			pathTransition.setOnFinished(event -> {elementIndex++;});
 
 			pathTransition.play();
 		}
--- src/main/java/de/slx/arcademenu/TransitionTest.java
+++ src/main/java/de/slx/arcademenu/TransitionTest.java
@@ -12,57 +12,61 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class TransitionTest extends Application {
-    Rectangle rect;
+	Rectangle rect;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        rect = new Rectangle(100, 100);
-        StackPane pane = new StackPane(rect);
-        Scene scene = new Scene(pane, 800, 800);
-        primaryStage.setScene(scene);
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		rect = new Rectangle(100, 100);
+		StackPane pane = new StackPane(rect);
+		Scene scene = new Scene(pane, 800, 800);
+		primaryStage.setScene(scene);
 
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, this::pressedKey);
-        primaryStage.show();
+		scene.addEventFilter(KeyEvent.KEY_PRESSED, this::pressedKey);
+		primaryStage.show();
 
-        new Thread(() -> {
-            while (true) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(rect.getLayoutX() + "\t/\t" + rect.getLayoutY() + "\t||\t" + rect.getTranslateX() + "\t/\t" + rect.getTranslateY());
-            }
-        }).start();
-    }
 
-    private void pressedKey(KeyEvent e) {
-        PathTransition pathTransition = new PathTransition();
+		new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(rect.getLayoutX() + "\t/\t" + rect.getLayoutY() + "\t||\t" + rect.getTranslateX() + "\t/\t" + rect.getTranslateY());
+			}
+		}).start();
+	}
 
-        Path path = new Path();
+	private void pressedKey(KeyEvent e) {
+		PathTransition pathTransition = new PathTransition();
 
-        rect.setLayoutX(rect.getTranslateX());
-        rect.setLayoutY(rect.getTranslateY());
-        rect.setTranslateX(0);
-        rect.setTranslateY(0);
-        path.getElements().add(new MoveTo(0,0));
+		Path path = new Path();
+		
+		rect.relocate(500, 500);
+		rect.setX(100);
+		rect.setY(100);
+		rect.setLayoutX(rect.getTranslateX());
+		rect.setLayoutY(rect.getTranslateY());
+		rect.setTranslateX(0);
+		rect.setTranslateY(0);
+		path.getElements().add(new MoveTo(50 + rect.getTranslateX(), 50 + rect.getTranslateY()));
 
-        if (e.getCode() == KeyCode.UP)
-            path.getElements().add(new LineTo(0, -100));
-        if (e.getCode() == KeyCode.DOWN)
-            path.getElements().add(new LineTo(0, 100));
-        if (e.getCode() == KeyCode.RIGHT)
-            path.getElements().add(new LineTo(100, 0));
-        if (e.getCode() == KeyCode.LEFT)
-            path.getElements().add(new LineTo(-100, 0));
+		if (e.getCode() == KeyCode.UP)
+			path.getElements().add(new LineTo(50 + rect.getTranslateX(), -50 + rect.getTranslateY()));
+		if (e.getCode() == KeyCode.DOWN)
+			path.getElements().add(new LineTo(50 + rect.getTranslateX(), 150 + rect.getTranslateY()));
+		if (e.getCode() == KeyCode.RIGHT)
+			path.getElements().add(new LineTo(150 + rect.getTranslateX(), 50 + rect.getTranslateY()));
+		if (e.getCode() == KeyCode.LEFT)
+			path.getElements().add(new LineTo(-50 + rect.getTranslateX(), 50 + rect.getTranslateY()));
 
-        pathTransition.setDuration(Duration.millis(1000));
-        pathTransition.setNode(rect);
-        pathTransition.setPath(path);
-        pathTransition.play();
-    }
+		pathTransition.setDuration(Duration.millis(100));
+		pathTransition.setNode(rect);
+		pathTransition.setPath(path);
+		pathTransition.play();
+	}
 }
